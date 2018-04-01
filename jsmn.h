a8c0ba3309bb6742579b9605fa9ce8b66956b67c
Leehyunji0715 <37644403+Leehyunji0715@users.noreply.github.com>
2018-04-01 17:28:47 +0900

    Update jsmn.h

--- jsmn.h
+++ jsmn.h
@@ -1,11 +1,11 @@
-#ifndef __JSMN_H_
-#define __JSMN_H_
+#ifndef __JSMN_H_//만약 JSMN_H가 선언되어있지않다면
+#define __JSMN_H_//이것을 선언한다
 
-#include <stddef.h>
+#include <stddef.h>//요거 선언한다
 
 #ifdef __cplusplus
-extern "C" {
-#endif
+extern "C" {//개념 : 전역변수 앞에 쓰이는 extern and static
+#endif//위 정의사항 끝!
 
 /**
  * JSON type identifier. Basic types are:
@@ -14,7 +14,7 @@ extern "C" {
  * 	o String
  * 	o Other primitive: number, boolean (true/false) or null
  */
-typedef enum {
+typedef enum {		//jsmntype_t 열거형(enum)!!!//아마 타입별로 번호를 나눠준것같음
 	JSMN_UNDEFINED = 0,
 	JSMN_OBJECT = 1,
 	JSMN_ARRAY = 2,
@@ -22,28 +22,29 @@ typedef enum {
 	JSMN_PRIMITIVE = 4
 } jsmntype_t;
 
-enum jsmnerr {
+enum jsmnerr {//jsmnerr 열거형
 	/* Not enough tokens were provided */
 	JSMN_ERROR_NOMEM = -1,
 	/* Invalid character inside JSON string */
 	JSMN_ERROR_INVAL = -2,
-	/* The string is not a full JSON packet, more bytes expected */
+	/* The string is not a full JSON packet, more bytes expected *///packet : 통, 소포
 	JSMN_ERROR_PART = -3
 };
 
 /**
+//형식이 이렇게 된다는 것을 알려주는것같으메
  * JSON token description.
  * type		type (object, array, string etc.)
  * start	start position in JSON data string
  * end		end position in JSON data string
  */
-typedef struct {
-	jsmntype_t type;
+typedef struct {// jsmntok_t 구조체 정의
+	jsmntype_t type;//type이란 이름으로 열거형 선언
 	int start;
 	int end;
 	int size;
-#ifdef JSMN_PARENT_LINKS
-	int parent;
+#ifdef JSMN_PARENT_LINKS//만약 얘도 정의되어 있다면
+	int parent;//parent도 선언
 #endif
 } jsmntok_t;
 
@@ -51,20 +52,20 @@ typedef struct {
  * JSON parser. Contains an array of token blocks available. Also stores
  * the string being parsed now and current position in that string
  */
-typedef struct {
-	unsigned int pos; /* offset in the JSON string */
-	unsigned int toknext; /* next token to allocate */
-	int toksuper; /* superior token node, e.g parent object or array */
+typedef struct {//jsmn_parser 구조체로 정의
+	unsigned int pos; /* offset상쇄하다 in the JSON string */
+	unsigned int toknext; /* next token to allocate *///토큰 : 문자열을 구분할수있는 단위
+	int toksuper; /* superior token node, e.g parent object or array *///상위 토큰
 } jsmn_parser;
 
 /**
- * Create JSON parser over an array of tokens
+ * Create JSON parser over an array of tokens(아래것 :연속적인것 -> 메모리할당을 하기위해 포인터로 선언)
  */
-void jsmn_init(jsmn_parser *parser);
+void jsmn_init(jsmn_parser *parser);//~~
 
 /**
  * Run JSON parser. It parses a JSON data string into and array of tokens, each describing
- * a single JSON object.
+ * a single JSON object.//parse : 분석하다.
  */
 int jsmn_parse(jsmn_parser *parser, const char *js, size_t len,
 		jsmntok_t *tokens, unsigned int num_tokens);
